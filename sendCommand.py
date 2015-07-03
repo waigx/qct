8477e7209d8c3564b6fe1e0bba13f8d143dac464
Yigong Wang <yigwang@cs.stonybrook.edu>
2015-07-03 02:55:57 -0400

    Fix the error in sendCommand.py

--- sendCommand.py
+++ sendCommand.py
@@ -11,12 +11,13 @@ import sys
 
 
 def sendCMD(cmd):
-	sp.call(['ssh', 'root@10.10.1.0', "\"" + cmd + "\""])
-	sp.call(['ssh', 'root@10.10.1.1', "\"" + cmd + "\""])
-	sp.call(['ssh', 'root@10.10.1.2', "\"" + cmd + "\""])
-	sp.call(['ssh', 'root@10.10.1.3', "\"" + cmd + "\""])
+	sp.call(['ssh', 'root@10.10.1.0', cmd])
+	sp.call(['ssh', 'root@10.10.1.1', cmd])
+	sp.call(['ssh', 'root@10.10.1.2', cmd])
+	sp.call(['ssh', 'root@10.10.1.3', cmd])
 	return;
 
 
 cmd = ' '.join(sys.argv[1:])
+print cmd
 sendCMD(cmd)
